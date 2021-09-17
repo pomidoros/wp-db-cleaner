a379982fb9a7f109bd261d637abf3a1d5e006e46
Slovenberg <islovenberg@mail.ru>
2021-09-17 19:12:44 +0300

    Development Main class: Connection

--- src/Main.php
+++ src/Main.php
@@ -4,12 +4,39 @@ namespace slovenberg\WpDbCleaner;
 
 
 class Main {
-	private static function findEnv() {
+	private static function findEnv() 
+	{
 
 	}
 
 	public static function start() {
 		$basic_dir = __DIR__ . "/../../../../";
-		var_dump(scandir($basic_dir));
+		static::checkEnvVars();
+		static::connectDb();
+	}
+
+	private static function checkEnvVars()
+	{
+		$var_names = ["DB_HOST", "DB_NAME", "DB_USER", "DB_PASSWORD"];
+		foreach($var_names as $var_name)
+		{
+			if(!isset($_ENV[$var_name]))
+			{
+				throw new \Exception("Hello");
+			}
+		}
+	}
+
+	private static function connectDb() 
+	{
+		try 
+		{
+			$connection = new \PDO("mysql:dbname=$_ENV[DB_NAME];host=$_ENV[DB_HOST]",
+				$_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+			$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		} catch(\Throwable $exc) 
+		{
+			echo $exc->getMessage() . PHP_EOL;
+		}
 	}
 }
